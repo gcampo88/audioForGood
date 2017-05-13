bc39e20a8978b0abf76a79408abe469ff86b3120
gcampo88 <gigi.campo@gmail.com>
2017-05-13 10:05:08 -0400

    separate out constants

---
+++ constants.js
@@ -0,0 +1,8 @@
+LOCATION = 'Location';
+PODCAST_NAME = 'Name of Podcast';
+PODCAST_LINK =  'Podcast Site URL';
+PODCAST_FEED = 'RSS Feed';
+CREATOR = 'Creator(s)/Lead Producer Name';
+EPISODE_NAME = 'Name of Episode';
+RECOMMENDATION = 'Recommendation Notes';
+RECOMMENDER = 'Team Six Assignee';
